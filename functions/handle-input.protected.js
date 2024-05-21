52c8269c0f5c7e264ec57a8ea8f63d802ea063c3
David Anderson <davidandersonsix@gmail.com>
2024-05-21 15:53:16 -0400

    Fixed up messaging implementation. Created a more detailed message and made implementation async

--- functions/handle-input.protected.js
+++ functions/handle-input.protected.js
@@ -1,45 +1,66 @@
-function sendText(context, to, message) {
+async function sendText(context, to, body) {
   // Get the Twilio client
   const client = context.getTwilioClient();
+  const from = context.TWILIO_PHONE_NUMBER;
 
-  // Create and send a new message
-  client.messages
-    .create({
-      body: message,
-      from: context.TWILIO_PHONE_NUMBER,
-      to: to,
-    })
-    .then((message) => console.log(`Sent message ${message.sid}`))
-    .done();
-};
+  try {
+    // Create and send a new message
+    const message = await client.messages.create({ body, from, to });
+    console.log(`Sent message ${message.sid}`);
+  } catch (error) {
+    console.error("Error sending text:", error);
+  }
+}
 
-exports.handler = function (context, event, callback) {
+exports.handler = async function (context, event, callback) {
   const twiml = new Twilio.twiml.VoiceResponse();
+  const voice = "Polly.Joey";
   const digit = event.Digits;
 
-  switch (digit) {
-    case '1':
-      console.log('Sending text...');
-      sendText(context, event.From, 'Hello from The Company! This is our dummy information.');
-      twiml.say({ voice: 'alice' }, 'Thank you for your request. You will receive a text message shortly.');
-      break;
-    case '2':
-      console.log('Recording message...');
-      twiml.say({ voice: 'alice' }, 'Please leave a message after the beep.');
-      break;
-    case '3':
-      console.log('Connecting caller to owner...');
-      sendText(context, context.MY_PHONE_NUMBER, `Incoming call from ${event.From}. Please standby.`);
-      twiml.say({ voice: 'alice' }, 'Connecting you to the owner. Please hold.');
-      twiml.pause({ length: 2 });
-      twiml.dial(context.MY_PHONE_NUMBER);
-      break;
-    default:
-      console.log('Invalid input.');
-      twiml.say({ voice: 'alice' }, 'Invalid input. Please try again.');
-      twiml.redirect('voice-ivr');
-  }
+  try {
+    switch (digit) {
+      case "1":
+        console.log("Sending text...");
+        const message =
+          "" +
+          "Hello from Red Oxford Online!\n" +
+          "We create high quality websites that your clients will love.\n\n" +
+          "Red Oxford Online is based in Waterloo Ontario, but we work with clients across North America. We're available 10AM and 6PM EDT, Monday - Friday.\n\n" +
+          "Our services: \n"+
+          "👉️ Website design\n"+
+          "👉️ Server & Backened Development\n"+
+          "👉️ Web Hosting\n"+
+          "👉️ Custom Domain Email\n"+
+          "👉️ Google Ad Campaigns & SEO\n\n"+ 
+          "We offer flexible payment options for new sites and site redesigns, with options starting at $0!\n\n" +
+          "Want to know more? Check out our website at redoxfordonline.com!\n"+
+          "Thanks for reaching out, we hope to hear from you soon!";
+        await sendText(context, event.From, message);
+        twiml.say(
+          { voice },
+          "You will receive a text message shortly. Thank you for calling us today, goodbye!"
+        );
+        break;
+      case "2":
+        console.log("Recording message...");
+        twiml.say({ voice }, "Please leave a message after the beep.");
+        break;
+      case "3":
+        console.log("Connecting caller to owner...");
+        twiml.say({ voice }, "Connecting you to the owner. Please hold.");
+        twiml.pause({ length: 1 });
+        twiml.dial(context.MY_PHONE_NUMBER);
+        break;
+      default:
+        console.log("Invalid input.");
+        twiml.say({ voice }, "Invalid input. Please try again.");
+        twiml.redirect("voice-ivr");
+    }
 
-  twiml.hangup();
-  callback(null, twiml);
+    twiml.hangup();
+    callback(null, twiml);
+  } catch (error) {
+    console.error("Error handling input:", error);
+    callback(error);
+  }
 };
