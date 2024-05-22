f4f13342595027089e14792ab1051dfb6dcd3f9e
David Anderson <davidandersonsix@gmail.com>
2024-05-22 14:25:15 -0400

    Updated whisper to reduce time taken to complete whisper.

--- functions/whisper.protected.js
+++ functions/whisper.protected.js
@@ -1,5 +1,5 @@
 exports.handler = async function (context, event, callback) {
     const twiml = new Twilio.twiml.VoiceResponse();
-    twiml.say({ voice: 'Polly.Joey' }, 'Red Oxford Online! This is a business call, please respond accordingly');
+    twiml.say({ voice: 'Polly.Joey' }, 'Red Oxford Online! Please respond accordingly');
     callback(null, twiml);
   };
