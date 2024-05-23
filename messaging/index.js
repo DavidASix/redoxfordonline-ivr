42d6d0874f4b12868c2bbb15cc89371b47922bef
David Anderson <davidandersonsix@gmail.com>
2024-05-23 15:21:55 -0400

    Added route to handle messaging features

---
+++ messaging/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const sendTextRouter = require('./send-text');
+
+router.use('/send-text', sendTextRouter);
+
+module.exports = router;
---
+++ messaging/send-text.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const Twilio = require("twilio");
+require("dotenv").config();
+
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const client = new Twilio(accountSid, authToken);
+
+router.post("/", async (req, res) => {
+  const to = req.body.to;
+  const body = req.body.body;
+  const from = process.env.TWILIO_PHONE_NUMBER;
+  console.log({ to, from, body });
+  try {
+    // Create and send a new message
+    const message = await client.messages.create({ body, from, to });
+    res.send(`Sent message ${message.sid}`);
+  } catch (error) {
+    console.error("Error sending text:", error);
+    res.status(500).send("Error sending text");
+  }
+});
+
+module.exports = router;
